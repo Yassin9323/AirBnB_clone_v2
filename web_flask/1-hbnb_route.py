174c8dc1efceeb549d24a10860e67f4d32c77fa2
Yassin9323 <yassin.waleed94@gmail.com>
2024-06-05 08:51:05 +0300

    T2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,6 +16,7 @@ def hello_test():
     """ func to print Hello HBNB !"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """ Func print HBNB in Web page """
