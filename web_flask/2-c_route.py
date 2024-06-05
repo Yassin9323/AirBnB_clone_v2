207ef1238507181d54ca5714137b5bd637b91a23
Yassin9323 <yassin.waleed94@gmail.com>
2024-06-05 09:04:55 +0300

    T2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    This is a script that starts a Flask web application
+    listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_test():
+    """ func to print Hello HBNB !"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ Func print HBNB in Web page """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_C(text):
+    """ Func print C with input in Web page """
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
